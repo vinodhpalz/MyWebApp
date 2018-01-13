14b2fca3b281656a79b01ab19d74ff982c88eba6
vinodhpl <31344189+vinodhpl@users.noreply.github.com>
2018-01-13 12:44:00 +0530

    First Commit

---
+++ src/main/java/com/vinodh/first/FirstServlet.java
@@ -0,0 +1,46 @@
+package com.vinodh.first;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class FirstServlet
+ */
+public class FirstServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public FirstServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+			String eid = request.getParameter("eid");
+			if(eid.equals("100"))
+			{
+				RequestDispatcher rd = request.getRequestDispatcher("success.jsp");
+				rd.forward(request, response);
+			}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,11 @@
+<html>
+<body>
+<h2>Hello World!</h2>
+<form action="FirstServlet" method="get">
+
+<input type="text" name="eid"/>
+<input type="submit"/> 
+
+</form>
+</body>
+</html>
